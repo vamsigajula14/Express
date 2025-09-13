6672dc8259823586205addb202c8b98bdfcf157d
Vamsi <146840998+vamsigajula14@users.noreply.github.com>
2025-09-13 06:02:05 +0530

    Update index.js

--- Sum/index.js
+++ Sum/index.js
@@ -2,46 +2,60 @@ const express = require('express');
 
 const app = express();
 
-function middleware(req,res,next){
-    const a = parseInt(req.query.a);
-    const b = parseInt(req.query.b);
-    const operation = req.path;
-    alert(operation);
-    if(operation == '/add'){
-        next(a + b);
-    }
-    else if(operation == '/sub'){
-        next(a - b);
-    }
-    else if(operation == '/mul'){
-        next(a * b);
-    }
-    else if(operation == '/div'){
-        next(a / b);
-    }
-    else{
-        res.status(404).send("You hit an incorrect URL");
-    }
 
-}
-app.use(middleware);
-
-app.get('/add',(req,res,val)=>{
-    res.status(200).send(val);
+app.get('/add',(req,res)=>{
+    try{
+        const a = req.query.a;
+        const b = req.query.b;
+        const val = parseInt(a) + parseInt(b);
+        res.status(200).json({
+            result : val
+        });
+    }catch(err){
+        res.status(400).send("Invalid Input");
+    }
 });
 
-app.get('/sub',(req,res,val)=>{
-    res.status(200).send(val);
+app.get('/sub',(req,res)=>{
+    try{
+        const a = req.query.a;
+        const b = req.query.b;
+        const val = parseInt(a) - parseInt(b);
+        res.status(200).json({
+            result : val
+        });
+    }catch(err){
+        res.status(400).send("Invalid Input");
+    }
 });
 
-app.get('/mul',(req,res,val)=>{
-    res.status(200).send(val);
+app.get('/mul',(req,res)=>{
+    try{
+        const a = req.query.a;
+        const b = req.query.b;
+        const val = parseInt(a) * parseInt(b);
+        res.status(200).json({
+                result : val
+        });
+    }catch(err){
+        res.status(400).send("Invalid Input");
+    }
 });
 
 
-app.get('/div',(req,res,val)=>{
-    res.status(200).send(val);
+app.get('/div',(req,res)=>{
+    try{
+        const a = req.query.a;
+        const b = req.query.b;
+        const val = parseInt(a) / parseInt(b);
+        res.status(200).json({
+            result : val
+        });
+    }catch(err){
+        res.status(400).send("Invalid Input");
+    }
 });
 
 app.listen(3000);
+
 
